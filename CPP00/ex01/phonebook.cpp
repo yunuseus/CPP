d3965e4065f7d6dcc33486e09db49bc191aeaabe
Yunus Emre Alp <yalp@k1m02s07.42istanbul.com.tr>
2025-06-26 18:13:58 +0300

    m

--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -1,5 +1,46 @@
 #include "phonebook.hpp"
 #include <iostream>
+#include <stdlib.h>
+
+size_t ft_strlen(std:: string s1)
+{
+	size_t i = 0;
+	while (s1[i] != '\0')
+		i++;
+	return i;
+}
+
+int	ft_strcmp(std:: string s1, std:: string s2, size_t n)
+{
+	size_t	i;
+	char	*a;
+	char	*b;
+
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void selection(std:: string select)
+{
+	if (ft_strcmp(select, "ADD", ft_strlen(select)) == 0)
+	{
+		std:: cout << "add yapılıyor" << std:: endl;
+	}
+	if (ft_strcmp(select, "SEARCH", ft_strlen(select)) == 0)
+	{
+		std:: cout << "search yapılıyor" << std:: endl;
+	}
+	if (ft_strcmp(select, "EXIT", ft_strlen(select)) == 0)
+	{
+		std:: cout << "exit yapılıyor" << std:: endl;
+	}
+}
 
 int main()
 {
@@ -13,6 +54,7 @@ int main()
 	
 		std:: cout << ""<< std::endl;
 		std:: cin >> select;
-		std:: cout << "girdiğiniz select" << select << std:: endl;
+		std:: cout << std:: endl;
+		selection(select);
 	}
 }
