4cb0ce4e5927285ec1f2d9c4d2954937aef149cd
Yunus Emre Alp <yalp@k1m32s10.42istanbul.com.tr>
2025-08-18 18:48:56 +0300

    m

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -9,6 +9,6 @@ std::cout << a << std::endl;
 std::cout << a++ << std::endl;
 std::cout << a << std::endl;
 std::cout << b << std::endl;
-
+std::cout << Fixed::max( a, b ) << std::endl;
 return 0;
 }
---
+++ cpp02/ex03/Fixed.hpp
@@ -0,0 +1,57 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+class Fixed
+{
+	private:
+	int value;
+	static const int fractionalBits = 8;
+	public:
+	Fixed(const Fixed& other);
+	Fixed();
+	Fixed(float a);
+	Fixed(int a);
+	~Fixed();
+	float toFloat(void) const;
+	int toInt(void) const;
+	int getRawBits(void) const;
+	void setRawBits(int a);
+	Fixed& operator=(const Fixed& a);
+	Fixed& operator++();
+	Fixed operator++(int);
+	Fixed& operator--();
+	Fixed operator--(int);
+	Fixed operator+(const Fixed& a) const;
+    Fixed operator-(const Fixed& a) const;
+    Fixed operator*(const Fixed& a) const;
+    Fixed operator/(const Fixed& a) const;
+	bool operator<(const Fixed&a) const;
+	bool operator>(const Fixed& a) const;
+	bool operator==(const Fixed& a) const;
+	bool operator<=(const Fixed& a) const;
+	bool operator>=(const Fixed& a) const;
+	bool operator!=(const Fixed& a) const;
+	static Fixed&       min(Fixed& a, Fixed& b);
+    static const Fixed& min(const Fixed& a, const Fixed& b);
+    static Fixed&       max(Fixed& a, Fixed& b);
+    static const Fixed& max(const Fixed& a, const Fixed& b);
+
+	 
+
+};
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+class Point
+{
+	private:
+		const Fixed x;
+		const Fixed y;
+	public:
+		Point();
+		Point(const Fixed a,  const Fixed b);
+		~Point();
+};
+
+#endif
