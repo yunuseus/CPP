a933077fc77d971ca6ab82c37cc5b7ed64a0fde6
yunuseus <134627365+yunuseus@users.noreply.github.com>
2026-05-20 09:05:16 +0300

    .
    .

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -1,40 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yalp <yalp@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 14:38:02 by yalp              #+#    #+#             */
-/*   Updated: 2025/10/29 15:02:39 by yalp             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
 
-#include <string>
-
+#include <iostream>
 class Bureaucrat
 {
-    private:
+private:
     const std::string name;
-    int grade;
-    public:
+    int grade; 
+public:
     Bureaucrat();
     Bureaucrat(std::string name, int grade);
     Bureaucrat(const Bureaucrat& other);
     Bureaucrat& operator=(const Bureaucrat& other);
     ~Bureaucrat();
-    std::string getName();
-    int getGrade();
+
+    std::string getName() const;
+    int getGrade() const;
+    void incrementGrade();
+    void decrementGrade();
     class GradeTooHighException : public std::exception
     {
-        public:
-        virtual const char* what() const throw();
-    };  
+    public:
+        const char* what() const throw();
+    };
     class GradeTooLowException : public std::exception
     {
-        public:
-        virtual const char* what() const throw();
-    };  
+    public:
+        const char* what() const throw();
+    };
 };
-
-std::ostream& operator<<(std::ostream& os, Bureaucrat& bureaucrat);
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+#endif
