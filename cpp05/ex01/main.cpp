3683ed8e4fb3d4b3e428ec497ca8b7faaf5fffde
yunuseus <134627365+yunuseus@users.noreply.github.com>
2026-05-20 09:06:28 +0300

    Update main.cpp

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -1,27 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yalp <yalp@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 14:50:27 by yalp              #+#    #+#             */
-/*   Updated: 2025/10/29 15:05:49 by yalp             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Bureaucrat.hpp"
-#include <iostream>
-int main()
-{
-    try 
+#include "Form.hpp"
+int main(){
+    try
     {
-        Bureaucrat a("yunus", 42);
-        std::cout << a.getName() << ", bureaucrat grade " << a.getGrade() << std::endl;
+        Bureaucrat yunus("Yunus", 3);
+        Form form("Form1", 3, 2);
+        std::cout << yunus << std::endl;
+        std::cout << form << std::endl;
+        form.beSigned(yunus);
+        std::cout << form << std::endl;
     }
-    catch (const std::exception& e) 
+    catch (const std::exception& e)
     {
-        std::cout << e.what() << std::endl;
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
-
 }
