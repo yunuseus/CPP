62dd34ce22ad2976215f4820c94112959276b679
yunuseus <134627365+yunuseus@users.noreply.github.com>
2026-05-20 09:05:33 +0300

    Update main.cpp

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -1,27 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yalp <yalp@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 14:50:27 by yalp              #+#    #+#             */
-/*   Updated: 2025/10/29 15:05:49 by yalp             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Bureaucrat.hpp"
-#include <iostream>
-int main()
-{
-    try 
-    {
-        Bureaucrat a("yunus", 42);
-        std::cout << a.getName() << ", bureaucrat grade " << a.getGrade() << std::endl;
-    }
-    catch (const std::exception& e) 
-    {
-        std::cout << e.what() << std::endl;
-    }
 
+int main(){
+    try
+    {
+        Bureaucrat yunus("Yunus", 2);
+        Bureaucrat esra("Esra", 149);
+        std::cout << yunus << std::endl;
+        yunus.incrementGrade();
+        std::cout << yunus << std::endl;
+        std::cout << esra << std::endl;
+        esra.decrementGrade();
+        std::cout << esra << std::endl;
+        yunus.incrementGrade();
+        esra.decrementGrade();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 }
